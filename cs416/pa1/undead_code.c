6ea07b58c0ff3c63d66f58a288532a9e38ca59de
Matthew Lee <mattlee446@gmail.com>
2017-02-03 19:18:34 -0500

    init

---
+++ cs416/pa1/undead_code.c
@@ -0,0 +1,35 @@
+// Author: John-Austen Francisco
+// Date: 9 September 2015
+//
+// Preconditions: Appropriate C libraries
+// Postconditions: Generates Segmentation Fault for
+//                               signal handler self-hack
+
+// Student name: Matthew Lee
+// Ilab machine used: man.cs.rutgers.edu
+
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void segment_fault_handler(int signum)
+{
+        void* p;
+        p = (void *) &signum; //Use the signum to construct a pointer to flag on stored stack
+        p += 0x4c-0x10; //Increment pointer down to the stored PC
+        *(int *)p += 0x6; //Increment value at pointer by length of bad instruction
+}
+
+int main()
+{
+        int r2 = 0;
+
+        signal(SIGSEGV, segment_fault_handler);
+
+        r2 = *( (int *) 0 );
+
+        printf("I live again!\n");
+
+        return 0;
+}
+
